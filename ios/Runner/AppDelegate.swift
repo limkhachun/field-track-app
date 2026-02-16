b9a86bcde59743e0b3752b30a8ed99b988b9af98
KHACHUN <limkc_wm22@student.tarc.edu.my>
2026-02-16 15:26:04 +0800

    Update API keys for Google Maps integration in Android and iOS
    - Replaced the Google Maps API key in AndroidManifest.xml for updated access.
    - Added Google Maps library import and configured the API key in AppDelegate.swift for iOS support.
    - Enhanced comments for clarity on the integration steps.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+// 🟢 1. 引入 Google Maps 库
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // 🟢 2. 配置 iOS 专用的 API Key
+    GMSServices.provideAPIKey("AIzaSyDggQmKVTngNmq1-C_GJ64Tq9LqkCiBsuI")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
